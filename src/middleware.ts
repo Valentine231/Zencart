7ff6c1573ad49335089f88eb74a046a5d50d9eeb
valentine231 <ugwuvalentine519@gmail.com>
2026-03-14 14:45:47 +0100

    fix(middleware): update Clerk matcher to fix Vercel 500 error & fix orders page

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,16 +9,17 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-   if (isProtectedRoute(req)) {
-    await auth.protect(); 
-   }
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
 });
 
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|svg|webp|ico|css|js)).*)",
-    "/",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
